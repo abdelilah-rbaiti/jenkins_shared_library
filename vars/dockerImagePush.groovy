9ef6128df5f35b3b4d4fdab1f810ce94143053e6
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2023-08-29 12:23:23 +0100

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,13 +1,22 @@
-def call(String project, String ImageTag, String hubUser){
-     withCredentials([usernamePassword(
-             credentialsId: "docker",
-             usernameVariable: "USER",
-             passwordVariable: "PASS"
-     )]) {
-           sh """
-            echo \$PASS | docker login -u \$USER --password-stdin
-            docker image push ${hubUser}/${project}:${ImageTag}
-            docker image push ${hubUser}/${project}:latest
-        """  
- }
+//def call(String project, String ImageTag, String hubUser){
+    // withCredentials([usernamePassword(
+           //  credentialsId: "docker",
+         //    usernameVariable: "USER",
+       //      passwordVariable: "PASS"
+     //)]) {
+           //sh """
+         //   echo \$PASS | docker login -u \$USER --password-stdin
+       //     docker image push ${hubUser}/${project}:${ImageTag}
+     //       docker image push ${hubUser}/${project}:latest
+   //     """  
+ //}
+//}
+
+
+def call(String aws_account_id, String region, String ecr_repoName){
+    
+    sh """
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
 }
