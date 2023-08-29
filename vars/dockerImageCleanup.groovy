9db211667e5a4c96f77bd8c172785e98fed8ef91
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2023-08-29 14:32:21 +0100

    Update dockerImageCleanup.groovy

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -9,6 +9,6 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+     docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.eu-north-1.amazonaws.com/${ecr_repoName}:latest
     """
 }
