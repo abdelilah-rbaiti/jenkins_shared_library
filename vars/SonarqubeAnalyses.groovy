8f2dc1f31c0747ad219b68a1d7e5144ca5dcd45b
abdelilah-rbaiti <142924743+abdelilah-rbaiti@users.noreply.github.com>
2023-10-27 16:46:41 +0100

    Update SonarqubeAnalyses.groovy

--- vars/SonarqubeAnalyses.groovy
+++ vars/SonarqubeAnalyses.groovy
@@ -2,6 +2,8 @@ def call(credentialsId){
 
 withSonarQubeEnv(credentialsId: 'SONAR') {
          sh 'mvn clean verify sonar:sonar '
+        -Dsonar.host.url=http://localhost:9000 \
+        -Dsonar.login=sqp_6e790995fbde76b214fc934afeaf753d86039d7f'
         
     }
 }
